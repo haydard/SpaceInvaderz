c05287598aacbc744e99fc734c27e73d05c9b99f
fresh_dumbledore <fresh_dumbledore@cs.tu-berlin.de>
2014-11-19 16:00:38 +0100

    alpha1.1

---
+++ Includes/eventHandler.h
@@ -0,0 +1,15 @@
+/*
+ * eventHandler.h
+ *
+ *  Created on: 19.11.2014
+ *      Author: fresh_dumbledore
+ */
+
+#ifndef EVENTHANDLER_H_
+#define EVENTHANDLER_H_
+
+#define MOVEMENT 0
+
+void check_input();
+
+#endif /* EVENTHANDLER_H_ */
